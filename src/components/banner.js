3659adf596b9ccd62dc9fe27ff27750feddc2efc
dhanushp <dhanushp.work@gmail.com>
2020-04-17 03:42:52 +0530

    Added Banner

--- src/components/banner.js
+++ src/components/banner.js
@@ -4,13 +4,24 @@ import { ReactComponent as Duke } from "../assets/duke.svg";
 const Banner = () => {
   return (
     <div className='main'>
-        <div className='container-fluid'>
-            <div className='bg-color'>
-            <div className='bike'>
-                <Duke />
+        <div className='container'>
+            <div className='bike-container'>
+                <div className='bg-color'>
+                    <div className='bike'>
+                        <Duke />
+                    </div>
+                </div>
             </div>
+            <div className='text-container'>
+                <h1>
+                    <div className='line'>
+                        Own the <span>Experience</span>,
+                    </div>
+                    <div className='line'>
+                        <span>Ride</span> in style.
+                    </div>
+                </h1>
             </div>
-            
         </div>
     </div>
   );
